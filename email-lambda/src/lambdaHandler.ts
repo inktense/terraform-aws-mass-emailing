a3bc12fdc518e6574d15f9746a308430dec87a82
Bianca Popescu <inktense12@gmail.com>
2022-06-20 19:46:40 +0100

    Added S3 client to Lambda

--- email-lambda/src/lambdaHandler.ts
+++ email-lambda/src/lambdaHandler.ts
@@ -1,7 +1,15 @@
-import { Handler } from "aws-lambda"
+import { Handler, S3Event, Context } from "aws-lambda"
 
-export const handler: Handler = async () => {
+import { getS3Document } from "./services/S3"
+
+export const handler: Handler = async (event: S3Event, context: Context): Promise<void> => {
+
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+  console.log("event => ", event, "context => ", context)
   try {
+    await getS3Document(bucket, key)
     console.log("hello world")
   } catch (err) {
     console.error(`Error`)
