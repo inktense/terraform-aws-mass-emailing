d80fb8d776da41f0550e5fce0fa050b782bbd574
Bianca Popescu <inktense12@gmail.com>
2022-07-06 15:19:41 +0100

    Added S3 backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-202201108659685447"
-    key    = "terraform-emailing.tfstate"
-    region = "eu-west-2"
+    bucket  = "tf-state-202201108659685447"
+    key     = "terraform-emailing.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+    acl     = "private"
   }
 }
